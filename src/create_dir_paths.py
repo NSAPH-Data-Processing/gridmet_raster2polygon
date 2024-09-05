c57f5439da9ba79d0d01e1bcbd4860561fe6f0c7
audiracmichelle <audiracmichelle@gmail.com>
2024-09-05 07:30:03 -0400

    modified recursivity

--- src/create_dir_paths.py
+++ src/create_dir_paths.py
@@ -5,34 +5,33 @@ from omegaconf import DictConfig
 
 LOGGER = logging.getLogger(__name__)
 
-def create_subfolders_and_links(datapath, subfolder_dict, base_path="data"):
+def create_subfolders_and_links(datapath="data", folder_dict=None):
     """
     Recursively create subfolders and symbolic links.
     """
-    for subfolder_name, subfolder_link in subfolder_dict.items():
-        subfolder_path = os.path.join(base_path, datapath, subfolder_name)
+    if os.path.exists(datapath):
+        LOGGER.info(f"Folder {datapath} already exists.")
+    else:
+        os.mkdir(datapath)
+        LOGGER.info(f"Created data path {datapath}")
 
-        if isinstance(subfolder_link, DictConfig):
+    if isinstance(folder_dict, DictConfig):
+        for path, dict in folder_dict.items():
             # Recursive call for nested subfolders
-            create_subfolders_and_links(subfolder_name, subfolder_link, os.path.join(base_path, datapath))
-        else:
-            if os.path.exists(subfolder_path):
-                LOGGER.info(f"Subfolder {subfolder_path} already exists.")
-                continue
-            if subfolder_link is not None:
-                #convert relative path to absolute path
-                subfolder_link = os.path.abspath(subfolder_link)
-                os.symlink(subfolder_link, subfolder_path)
-                LOGGER.info(f"Created symlink {subfolder_path} -> {subfolder_link}")
-            else:
-                os.makedirs(subfolder_path, exist_ok=True)
-                LOGGER.info(f"Created subfolder {subfolder_path}")
+            create_subfolders_and_links(os.path.join(datapath, path), dict)
+    elif folder_dict is None:
+        LOGGER.info(f"End of recursive branch")
+    else:
+        #convert relative path to absolute path
+        folder_link = os.path.abspath(folder_dict)
+        os.symlink(folder_dict, datapath)
+        LOGGER.info(f"Created symlink {datapath} -> {folder_link}")
 
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg):
     """Create data subfolders and symbolic links as indicated in config file."""
-    for datapath, subfolder_dict in cfg.datapaths.items():
-        create_subfolders_and_links(datapath, subfolder_dict)
+    create_subfolders_and_links(folder_dict=cfg.datapaths)
 
 if __name__ == "__main__":
     main()
+
