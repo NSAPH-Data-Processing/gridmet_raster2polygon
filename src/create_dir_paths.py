f72b344fcaa4164d4f72644dda0fcac08723c25a
audiracmichelle <audiracmichelle@gmail.com>
2024-09-05 20:14:18 -0400

    optimizes create_dir_paths behaviour

--- src/create_dir_paths.py
+++ src/create_dir_paths.py
@@ -9,23 +9,29 @@ def create_subfolders_and_links(datapath="data", folder_dict=None):
     """
     Recursively create subfolders and symbolic links.
     """
-    if os.path.exists(datapath):
-        LOGGER.info(f"Folder {datapath} already exists.")
-    else:
-        os.mkdir(datapath)
-        LOGGER.info(f"Created data path {datapath}")
-
+    if not os.path.exists(datapath):
+        LOGGER.info(f"Error: {datapath} does not exists.")
+        return
     if isinstance(folder_dict, DictConfig):
-        for path, dict in folder_dict.items():
-            # Recursive call for nested subfolders
-            create_subfolders_and_links(os.path.join(datapath, path), dict)
-    elif folder_dict is None:
-        LOGGER.info(f"End of recursive branch")
-    else:
-        #convert relative path to absolute path
-        folder_link = os.path.abspath(folder_dict)
-        os.symlink(folder_dict, datapath)
-        LOGGER.info(f"Created symlink {datapath} -> {folder_link}")
+        for path, subfolder_dict in folder_dict.items():
+            sub_datapath = os.path.join(datapath, path)
+            if isinstance(subfolder_dict, str):
+                # Create symbolic link
+                if os.path.exists(sub_datapath):
+                    LOGGER.info(f"Symbolic link to {sub_datapath} already exists")
+                else:
+                    os.symlink(os.path.abspath(subfolder_dict), sub_datapath)
+                    LOGGER.info(f"Created symlink {sub_datapath} -> {subfolder_dict}")
+            else:
+                # Create subfolder
+                if os.path.exists(sub_datapath):
+                    LOGGER.info(f"Path {sub_datapath} already exists")
+                else:
+                    os.mkdir(sub_datapath)
+                    LOGGER.info(f"Created data path {sub_datapath}")
+                if subfolder_dict is not None:
+                    # Recursive call for nested subfolders
+                    create_subfolders_and_links(sub_datapath, subfolder_dict)
 
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg):
